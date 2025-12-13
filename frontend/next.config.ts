9ec3b7804a88db753f65e5cc1bf64b7663eb48cb
rohaaaaaan <rohankadam230101997@gmail.com>
2025-12-13 16:15:37 +0530

    Fix invalid options in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
